c26c7c5d0726ee20b3034a94a67cc17069665b86
bruno <bpires.siqueira@gmail.com>
2025-05-24 11:01:09 -0300

    insertion sort explicação

---
+++ insertion_sort.py
@@ -0,0 +1,7 @@
+# EXPLICAÇÃO
+
+# 1 - Suponha que o primeiro elemento na matriz já esteja classificado.
+# 2 - Percorra o array a partir do elemento na segunda posição e compare-o com o primeiro elemento já ordenado. Se o primeiro elemento for maior que o segundo, troque-os.
+# 3 - Compare o terceiro elemento com os dois anteriores e, se for menor, troque novamente.
+# 4 - Continue esse processo até que todo o array esteja classificado.
+
