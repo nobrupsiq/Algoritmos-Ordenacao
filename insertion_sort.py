c8748b78ab02bd6ab5968cc4ecf0097c9291cf97
bruno <bpires.siqueira@gmail.com>
2025-05-25 21:46:10 -0300

    insertion sort funcionando mas inacabado

--- insertion_sort.py
+++ insertion_sort.py
@@ -5,3 +5,18 @@
 # 3 - Compare o terceiro elemento com os dois anteriores e, se for menor, troque novamente.
 # 4 - Continue esse processo até que todo o array esteja classificado.
 
+def insertion_sort(lista):
+  lenght_lista = len(lista) # Tamanho da lista
+
+  for i in range(lenght_lista): # Looping do valor 0 até o tamanho da lista
+    primeiro_elemento = lista[i] # Como diz a explicação o primeiro elemento precisa ja está classificado
+    elemento_anterior = i -1 # Esse será o segundo elemento Exemplo: Se meu i é 3 o elemento_anterior será 2(Elemento atual e elemento anterior)
+
+    while(elemento_anterior >= 0) and (primeiro_elemento < lista[elemento_anterior]):
+      lista[elemento_anterior + 1] = lista[elemento_anterior]
+      elemento_anterior -= 1
+    lista[elemento_anterior + 1] = primeiro_elemento
+  return lista
+
+teste = [4,2,6,3,9,1]
+print(insertion_sort(teste))
