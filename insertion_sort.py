f7d7e1dd2c1258ec449713f76c57111a8cbc81f2
bruno <bpires.siqueira@gmail.com>
2025-05-25 21:51:46 -0300

    insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -6,17 +6,15 @@
 # 4 - Continue esse processo até que todo o array esteja classificado.
 
 def insertion_sort(lista):
-  lenght_lista = len(lista) # Tamanho da lista
-
-  for i in range(lenght_lista): # Looping do valor 0 até o tamanho da lista
-    primeiro_elemento = lista[i] # Como diz a explicação o primeiro elemento precisa ja está classificado
-    elemento_anterior = i -1 # Esse será o segundo elemento Exemplo: Se meu i é 3 o elemento_anterior será 2(Elemento atual e elemento anterior)
-
-    while(elemento_anterior >= 0) and (primeiro_elemento < lista[elemento_anterior]):
-      lista[elemento_anterior + 1] = lista[elemento_anterior]
-      elemento_anterior -= 1
-    lista[elemento_anterior + 1] = primeiro_elemento
-  return lista
-
-teste = [4,2,6,3,9,1]
-print(insertion_sort(teste))
+    n = len(lista)
+    for i in range(1, n): # loop para percorrer a lista.
+        chave = lista[i] # A chave recebe os objetos da lista.
+        j = i - 1 # aqui o J recebe uma casa atras da chave.
+        while j >= 0 and lista[j] > chave: 
+            lista[j + 1] = lista[j] # Aqui o objeto J recebe uma casa afrente da que ele esta.
+            j -= 1
+        lista[j + 1] = chave # a chave vai receber uma casa a frente do objeto J.
+    return lista
+lista = [1, 9, 4, 5, 2, 3]
+insertion_sort(lista)
+print(lista)
