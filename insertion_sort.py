365402b33caacc47d38c82c31c03be4449c5cb19
bruno <bpires.siqueira@gmail.com>
2025-06-03 16:27:05 -0300

    update insertion_sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,21 +1,14 @@
-# EXPLICAÇÃO
-
-# 1 - Suponha que o primeiro elemento na matriz já esteja classificado.
-# 2 - Percorra o array a partir do elemento na segunda posição e compare-o com o primeiro elemento já ordenado. Se o primeiro elemento for maior que o segundo, troque-os.
-# 3 - Compare o terceiro elemento com os dois anteriores e, se for menor, troque novamente.
-# 4 - Continue esse processo até que todo o array esteja classificado.
-
 def insertion_sort(lista):
     tamanho_da_lista = len(lista)
 
-    for i in range(1, tamanho_da_lista): # loop para percorrer a lista.
-        chave = lista[i] # A chave recebe os items da lista.
-        primeiro_elemento = i - 1 # aqui a variável peimeiro_elemento recebe uma casa atras da chave.
+    for i in range(1, tamanho_da_lista): 
+        chave = lista[i]
+        primeiro_elemento = i - 1
         while primeiro_elemento >= 0 and lista[primeiro_elemento] > chave: 
-            lista[primeiro_elemento + 1] = lista[primeiro_elemento] # Aqui o objeto primeiro_elemento recebe uma casa afrente da que ele esta.
+            lista[primeiro_elemento + 1] = lista[primeiro_elemento]
             primeiro_elemento -= 1
 
-        lista[primeiro_elemento + 1] = chave # a chave vai receber uma casa a frente do objeto primeiro_elemento.
+        lista[primeiro_elemento + 1] = chave
 
     return lista
 
