e4fb7bb80013ceeaf750df24da28abf9387c3c66
bruno <bpires.siqueira@gmail.com>
2025-06-03 17:14:04 -0300

    insertion sort update

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,14 +1,15 @@
 def insertion_sort(lista):
-    tamanho_da_lista = len(lista)
+    tamanho_da_lista = len(lista) # 5
 
     for i in range(1, tamanho_da_lista): 
-        chave = lista[i]
         primeiro_elemento = i - 1
-        while primeiro_elemento >= 0 and lista[primeiro_elemento] > chave: 
+        segundo_elemento = lista[i]
+        
+        while primeiro_elemento >= 0 and lista[primeiro_elemento] > segundo_elemento: 
             lista[primeiro_elemento + 1] = lista[primeiro_elemento]
             primeiro_elemento -= 1
 
-        lista[primeiro_elemento + 1] = chave
+        lista[primeiro_elemento + 1] = segundo_elemento
 
     return lista
 
