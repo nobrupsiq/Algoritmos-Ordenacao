88f7a3c6267ca4abef0053c0332d7c948ee78d65
bruno <bpires.siqueira@gmail.com>
2025-06-03 16:31:05 -0300

    merge_sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,35 +1,34 @@
 def merge_sort(lista):
-    if len(lista) <= 1: # se a lista for menor ou igual a 1. Ela retorna ela mesma .
+    if len(lista) <= 1: 
         return lista
     
 
-    div = len(lista) // 2   # Aqui divide a lista em duas partes.
-    esquerda = lista[:div]  # Metade da lista. Do inicio até antes do meio.
-    direita = lista[div:]   # Outra metade da lista. Do meio até o final.
+    div = len(lista) // 2   
+    esquerda = lista[:div]  
+    direita = lista[div:]  
 
-    sorted_esquerda = merge_sort(esquerda) # Ordena a parte da esquerda. Separada da direita.
-    sorted_direita = merge_sort(direita)   # Ordena a parte da direita. Separada da esquerda.
+    sorted_esquerda = merge_sort(esquerda) 
+    sorted_direita = merge_sort(direita)   
 
-    return merge(sorted_esquerda, sorted_direita) # Junta as duas listas. Na função marge.
+    return merge(sorted_esquerda, sorted_direita) 
 
 def merge(esquerd, direit):
-    entrada = [] # Lista vazia aonde vai receber os elementos já ordenados.
-    i = j = 0    # São os indices atuas da esquerda e direita, começando em zero.
+    entrada = [] 
+    i = j = 0  
 
     while i < len(esquerd) and j < len(direit):
-        if esquerd[i] < direit[j]: # Se o elemento da esquerda for menor.
-            entrada.append(esquerd[i]) # Ele adiciona o elemento da esquerda.
-            i = i + 1  # O indice i avança. do lado esquerdo.
+        if esquerd[i] < direit[j]: 
+            entrada.append(esquerd[i]) 
+            i = i + 1 
         else:
-            entrada.append(direit[j]) # Se o elemento da direita for menor ele e adicionado.
-            j = j + 1 # O indice j avança. Do lado direito.
+            entrada.append(direit[j]) 
+            j = j + 1 
     
-    entrada.extend(esquerd[i:]) # Se algum elemento do lado esquerdo tiver sobrado, ele adiciona no final da fila.
-    entrada.extend(direit[j:])  # Se algum elemento do lado direito tiver sobrado, ele adiciona no final da fila.
+    entrada.extend(esquerd[i:]) 
+    entrada.extend(direit[j:])  
 
-    return entrada # Retorna a lista ordenada.
+    return entrada 
 
-
-lista = [9,4,8,1,3,2]  # lista que vai ser ordenada.
-a = merge_sort(lista)  # Chama a função merge_sort.
-print(a)   # Exibe a lista ordenada.
+lista = [9,4,8,1,3,2]
+a = merge_sort(lista) 
+print(a)   
